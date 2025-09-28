fe4e08329147dbb66f81467f837d9e7d90ecdcac
IPPO34 <carlostristan154@gmail.com>
2025-09-28 17:38:13 -0500

    Frontend Big Plast listo

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
+const API_URL = "https://backend-bigplast.onrender.com/api";
 
 // 📦 Obtener productos
 export async function fetchProductos() {
